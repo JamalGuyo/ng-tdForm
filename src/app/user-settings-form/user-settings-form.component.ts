e947062dba725b80802f59b50296755eb581f400
JamalGuyo <jamalguyo08@gmail.com>
2021-01-27 15:30:15 +0300

    track form elements with ngModel

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from '../models/user-setting';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.scss']
+  styleUrls: ['./user-settings-form.component.scss'],
 })
 export class UserSettingsFormComponent implements OnInit {
+  userSettings: IUserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annually',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
